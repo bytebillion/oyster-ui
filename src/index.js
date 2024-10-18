8cd314f81f393b7a5b8625be0f7a8d3bd1e83b81
Sandeep Solanki <107045051+sandeeps02@users.noreply.github.com>
2024-10-18 17:25:15 +0530

    First Commit

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+
 ReactModal.setAppElement("#root");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
